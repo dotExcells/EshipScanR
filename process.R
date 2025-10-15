4508a255330dcdfda4df2ede0217fa7243ff7eb8
dotExcells <59560853+dotExcells@users.noreply.github.com>
2025-10-15 11:18:40 +0200

    Add files via upload

--- process.R
+++ process.R
@@ -18,6 +18,18 @@ ClassData = data.frame(read_excel(file_path))
 #trims off useless form data
 ClassData <- ClassData[ -c(2:6) ]
 
+#assign names to numeric score columns
+score_cols <- 3:ncol(ClassData)  # first 2 cols MUST be ID + ParticipantCode
+names(ClassData)[score_cols] <- c(
+  paste0("Inter", 1:3),
+  paste0("Div", 1:4),
+  paste0("Sys", 1:4),
+  paste0("Stra", 1:8),
+  paste0("Norm", 1:4),
+  paste0("Fores", 1:4)
+)
+
+
 #sets the ID to count from just in case you started from a row ID =/= 1
 CurrentIndividual <- ClassData[1,1]
 CurrentIndividual <- strtoi(CurrentIndividual) # needs to be an intager
@@ -25,35 +37,34 @@ NumberOfIndividuals <- nrow(ClassData)
 FinalIndividual <- NumberOfIndividuals+CurrentIndividual
 
 #calculates averages for each category and adds to ClassData sheet
-ClassData$IntAv <- rowMeans(ClassData[ , c(3:5)], na.rm=TRUE)
-ClassData$DivAv <- rowMeans(ClassData[ , c(6:9)], na.rm=TRUE)
-ClassData$SysAv <- rowMeans(ClassData[ , c(10:13)], na.rm=TRUE)
-ClassData$StraAv <- rowMeans(ClassData[ , c(14:21)], na.rm=TRUE)
-ClassData$NormAv <- rowMeans(ClassData[ , c(22:25)], na.rm=TRUE)
-ClassData$ForesAv <- rowMeans(ClassData[ , c(26:29)], na.rm=TRUE)
+ClassData$IntAv   <- rowMeans(ClassData[, grep("^Inter", names(ClassData))], na.rm=TRUE)
+ClassData$DivAv   <- rowMeans(ClassData[, grep("^Div", names(ClassData))], na.rm=TRUE)
+ClassData$SysAv   <- rowMeans(ClassData[, grep("^Sys", names(ClassData))], na.rm=TRUE)
+ClassData$StraAv  <- rowMeans(ClassData[, grep("^Stra", names(ClassData))], na.rm=TRUE)
+ClassData$NormAv  <- rowMeans(ClassData[, grep("^Norm", names(ClassData))], na.rm=TRUE)
+ClassData$ForesAv <- rowMeans(ClassData[, grep("^Fores", names(ClassData))], na.rm=TRUE)
 
 #stores class averages as values for later
-IntAv <- mean(ClassData[,30])
-DivAv <- mean(ClassData[,31])
-SysAv <- mean(ClassData[,32])
-StraAv <- mean(ClassData[,33])
-NormAv <- mean(ClassData[,34])
-ForesAv <- mean(ClassData[,35])
+IntAv   <- mean(ClassData$IntAv)
+DivAv   <- mean(ClassData$DivAv)
+SysAv   <- mean(ClassData$SysAv)
+StraAv  <- mean(ClassData$StraAv)
+NormAv  <- mean(ClassData$NormAv)
+ForesAv <- mean(ClassData$ForesAv)
 
 #loop the following code until all individuals are processed
-while(CurrentIndividual < FinalIndividual) { 
-
-#select the (current) top row
-  SelectedIndividual <- ClassData %>% filter(ID==CurrentIndividual)
-
-#get the personal averages
-  InterpersonalMean <- round(SelectedIndividual[1,30], digits = 1)
-  DiversityMean <- round(SelectedIndividual[1,31], digits = 1)
-  SysThinkMean <- round(SelectedIndividual[1,32], digits = 1)
-  StratActionMean <- round(SelectedIndividual[1,33], digits = 1)
-  NormCompMean <- round(SelectedIndividual[1,34], digits = 1)
-  ForesThinkMean <- round(SelectedIndividual[1,35], digits = 1)
+for(i in 1:nrow(ClassData)) {
+  SelectedIndividual <- ClassData[i, ]
   
+  InterpersonalMean <- round(SelectedIndividual$IntAv, 1)
+  DiversityMean     <- round(SelectedIndividual$DivAv, 1)
+  SysThinkMean      <- round(SelectedIndividual$SysAv, 1)
+  StratActionMean   <- round(SelectedIndividual$StraAv, 1)
+  NormCompMean      <- round(SelectedIndividual$NormAv, 1)
+  ForesThinkMean    <- round(SelectedIndividual$ForesAv, 1)
+  
+  idName <- SelectedIndividual$ParticipantCode  # adjust if needed
+
 #make the axis titles for each person include their scores
   InterpersonalTitle = toString(paste("Interpersonal -", InterpersonalMean))
   DiversityTitle = toString(paste("Diversity -", DiversityMean))
@@ -70,7 +81,15 @@ while(CurrentIndividual < FinalIndividual) {
   #reformat the labels to look nice
   Plottabledata <- rename(Plottabledata, !!InterpersonalTitle := InterpersonalMean, !!DiversityTitle := DiversityMean, !!SysThinkTitle := SysThinkMean, !!StratActionTitle := StratActionMean, !!NormCompTitle := NormCompMean, !!ForesThinkTitle := ForesThinkMean,)
   ReferenceData <- replace(ReferenceData, 1, "Class Average")
-  Plottabledata[nrow(Plottabledata) + 1,] <- c(ReferenceData, IntAv, DivAv, SysAv, StraAv, NormAv, ForesAv)
+  Plottabledata[nrow(Plottabledata) + 1, ] <- data.frame(
+    idName = "Class Average",
+    InterpersonalMean = round(IntAv, 1),
+    DiversityMean     = round(DivAv, 1),
+    SysThinkMean      = round(SysAv, 1),
+    StratActionMean   = round(StraAv, 1),
+    NormCompMean      = round(NormAv, 1),
+    ForesThinkMean    = round(ForesAv, 1)
+  )
   Plottabledata <- as.data.frame(Plottabledata)
   
 #make a jpeg file container
